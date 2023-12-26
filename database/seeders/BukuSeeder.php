a821b51eb9ee01fc7ee121a632af692db95552a8
aldimhmmdr <ramdani.aldi93@gmail.com>
2023-12-26 16:21:56 +0700

    create migrate, create breeze

---
+++ database/seeders/BukuSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Books;
+use App\Models\Writter;
+use App\Models\Category;
+use App\Models\Publishers;
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $seeder = [
+        ];
+       
+    
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,34 +17,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/home', function () {
-    return view('home');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/genre', function () {
-    return view('dashboard.user.genre.index');
-});
-Route::get('/mybook', function () {
-    return view('dashboard.user.mybook.index');
-});
-Route::get('/favorite', function () {
-    return view('dashboard.user.favorite.index');
-});
-Route::get('/home', function () {
-    return view('dashboard.user.index');
-});
-Route::get('/login', function () {
-    return view('login.index');
-});
-Route::get('/register', function () {
-    return view('register.index');
-});
-// admin
-Route::get('/admin', function () {
-    return view('dashboard.admin.layout.main');
-});
-Route::get('/admin/users', function () {
-    return view('dashboard.admin.users.index');
-});
+
+require __DIR__.'/auth.php';
