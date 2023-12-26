ba0e888843fe4e2b4302ff7fdcd5bb9d70a4780a
aldimhmmdr <ramdani.aldi93@gmail.com>
2023-12-26 19:18:36 +0700

    update genre controller

--- app/Http/Controllers/GenreController.php
+++ app/Http/Controllers/GenreController.php
@@ -3,64 +3,59 @@
 namespace App\Http\Controllers;
 
 use App\Models\Genre;
-use App\Http\Requests\StoreGenreRequest;
-use App\Http\Requests\UpdateGenreRequest;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class GenreController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
-        //
+        $genre = Genre::paginate(5);
+        return view('dashboard.admin.genre.index', compact('genre'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
     public function create()
     {
-        //
+        return view('dashboard.admin.genre.create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreGenreRequest $request)
+    public function store(Request $request)
     {
-        //
+        $request->validate([
+            'judul' => 'required|max:255|min:3',
+        ]);
+
+        Genre::create($request->all());
+
+        return redirect('/dashboard/genre')->with('success', 'Data Berhasil Ditambahkan!');
     }
 
-    /**
-     * Display the specified resource.
-     */
     public function show(Genre $genre)
     {
-        //
+        return view('dashboard.admin.genre.show', compact('genre'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
     public function edit(Genre $genre)
     {
-        //
+        return view('dashboard.admin.genre.edit', compact('genre'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateGenreRequest $request, Genre $genre)
+    public function update(Request $request, Genre $genre)
     {
-        //
+        $request->validate([
+            'judul' => 'required|max:255|min:3',
+        ]);
+
+        Genre::where('id', $genre->id)
+            ->update([
+                'judul' => $request->judul,
+            ]);
+        return redirect('/dashboard/genre')->with('success', 'Data Berhasil Diubah!');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     */
     public function destroy(Genre $genre)
     {
-        //
+        Genre::destroy($genre->id);
+        return redirect()->route('dashboard.admin.genre.index')->with('success', 'Genre berhasil dihapus.');
     }
 }
