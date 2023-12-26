55c827f648ec773d63373a127903ba7721807414
MutiaraLF26 <mutiara.213040079@mail.unpas.ac.id>
2023-12-26 22:18:17 +0700

    menambahkan database

--- app/Http/Controllers/NovelController.php
+++ app/Http/Controllers/NovelController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Novel; // Tambahkan use statement untuk model Novel
 
 class NovelController extends Controller
 {
-    //
+    /**
+     * Menampilkan daftar novel.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $novels = Novel::latest()->paginate(5);
+        return view('novel.index', compact('novels'));
+    }
 }
