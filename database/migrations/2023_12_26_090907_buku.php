7bf7285922926802a36c37544a4899ae41d406f9
aldimhmmdr <ramdani.aldi93@gmail.com>
2023-12-26 16:32:52 +0700

    update buku migrate

--- database/migrations/2023_12_26_090907_buku.php
+++ database/migrations/2023_12_26_090907_buku.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('buku', function (Blueprint $table) {
             $table->id();
+            $table->string('judul');
+            $table->string('gambar');
+            $table->text('deskripsi');
+            $table->string('views');
+            $table->string('suka');
+            $table->string('halaman');
+            $table->string('favorit');
+            $table->integer('user_id');
+            $table->integer('genre_id');
             $table->timestamps();
         });
     }
