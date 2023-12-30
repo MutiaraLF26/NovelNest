1d6cefa6b8f26eaa3b4a118f1dcaee1dc84efaba
rikawenda <febriyantirika081@gmail.com>
2023-12-30 21:34:52 +0700

    Membenarkan Error

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-<<<<<<< HEAD
-use App\Http\Controllers\BukuController;
-use App\Http\Controllers\GenreController;
-use App\Http\Controllers\ProfileController;
-=======
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
 use Illuminate\Support\Facades\Auth;
->>>>>>> 48121fdc9657c380445fe1c713a54e2fdf538d14
-=======
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\AdminController;
-use Illuminate\Support\Facades\Auth;
->>>>>>> 48121fdc9657c380445fe1c713a54e2fdf538d14
 
 /*
 |--------------------------------------------------------------------------
@@ -27,6 +15,7 @@ use Illuminate\Support\Facades\Auth;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
 Route::get('/', function () {
     return view('welcome');
 })->name('/');
@@ -35,6 +24,13 @@ Route::get('/bestSeller', function () {
 });
 Route::get('/feedback', function () {
     return view('feedback');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/about', function () {
+    return view('about');
 });
 Route::get('/genre', function () {
     return view('dashboard.user.genre.index');
@@ -73,53 +69,4 @@ Route::middleware(['auth', 'role:admin'])->group(function() {
 
 Route::controller(AdminController::class,)->group(function() {
     Route::post('/admin/users/store', 'storeUser')->name('storeUser');
-<<<<<<< HEAD
 });
-Route::get('/feedback', function () {
-    return view('feedback');
-});
-Route::get('/feedback', function () {
-    return view('feedback');
-=======
->>>>>>> 48121fdc9657c380445fe1c713a54e2fdf538d14
-});
-Route::get('/about', function () {
-    return view('about');
-});
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/buku', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::get('/genre', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-Route::middleware(['auth'])->group(function () {
-    Route::get('/buku', [BukuController::class, 'index'])->name('buku.index');
-    Route::get('/buku/{buku}', [BukuController::class, 'show'])->name('buku.show');
-    Route::get('/buku/create', [BukuController::class, 'create'])->name('buku.create');
-    Route::post('/buku', [BukuController::class, 'store'])->name('buku.store');
-    Route::get('/buku/{buku}/edit', [BukuController::class, 'edit'])->name('buku.edit');
-    Route::patch('/buku/{buku}', [BukuController::class, 'update'])->name('buku.update');
-    Route::delete('/buku/{buku}', [BukuController::class, 'destroy'])->name('buku.destroy');
-});
-
-Route::middleware(['auth', 'admin'])->group(function () {
-    Route::get('/genre', [GenreController::class, 'index'])->name('genre.index');
-    Route::get('/genre/{genre}', [GenreController::class, 'show'])->name('genre.show');
-    Route::get('/genre/create', [GenreController::class, 'create'])->name('genre.create');
-    Route::post('/genre', [GenreController::class, 'store'])->name('genre.store');
-    Route::get('/genre/{genre}/edit', [GenreController::class, 'edit'])->name('genre.edit');
-    Route::patch('/genre/{genre}', [GenreController::class, 'update'])->name('genre.update');
-    Route::delete('/genre/{genre}', [GenreController::class, 'destroy'])->name('genre.destroy');
-});
-require __DIR__.'/auth.php';
